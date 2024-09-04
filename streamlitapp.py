d3a54ffa578f35abb6973acb1e53912fd84ba320
Laibakhurshid903 <laibakhurshid903@gmail.com>
2024-09-04 11:46:56 +0000

    Added my streamlit app

---
+++ streamlitapp.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+import joblib
+import sklearn.metrics import acurracy_score 
+from sklearn.model_selection import train_test_split
+import streamlit as st
+model = joblib.load("liveModelV1.pk1")
+data = pd.read_csv('mobile_price_range_data.csv')
+x = data.iloc[: ,-1]
+y = data.iloc[:, -1]
+x_train , x_test, y_train, y_test train_test_split (x , y, test_size =0.2, random_state = 42)
+#make prediction for x_test
+y_pred = model.pred(x_test)
+#calculate accuracy \
+accuracy = accuracy_score(y_test, y_pred)
+#page title 
+st.title("Model Accuracy and Real-Time Prediction")
+#Display accuracy 
+st.write(f"Model{accuracy}")
+#real time prediction based on user input
+st.header("Real-Time Prediction")\
+input_data = []
+for col in x_test.columns:
+    input_value = st.number_input(f'input for feature {col}', value =''
+input_data.append(input_value))
+#convert input data to dataframe 
+input_df = pd.DataFrame([input_data],columns=x_test.columns)
+#make predictions 
+if st.button("Predict"):
+    prediction = model.predict(input_df)
+    st.write(f'Prediction:{prediction[0]}')
