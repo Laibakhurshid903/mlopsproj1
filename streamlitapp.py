0c616d81cf63651955a2e18bbce6a383d38f2ac9
Laibakhurshid903 <laibakhurshid903@gmail.com>
2024-09-04 12:26:17 +0000

    Added streamlit app

--- streamlitapp.py
+++ streamlitapp.py
@@ -21,7 +21,7 @@ st.header("Real-Time Prediction")
 input_data = []
 for col in x_test.columns:
     input_value = st.number_input(f'input for feature {col}', value =0 
-input_data.append(input_value))
+    input_data.append(input_value))
 #convert input data to dataframe 
 input_df = pd.DataFrame([input_data],columns=x_test.columns)
 #make predictions 
